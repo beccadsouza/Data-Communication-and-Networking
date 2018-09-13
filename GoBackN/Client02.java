4c1dbbcf1d1f02f131f02579da771da836a0f539
Rebecca D'souza <beccadsouza16@gmail.com>
2018-09-13 23:07:02 +0530

    starting go back n

--- GoBackN/Client02.java
+++ GoBackN/Client02.java
@@ -1,29 +1,24 @@
-package DCCN.arq;
+package DCCN.GoBackN;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /*
  * Created by Rebecca D'souza on 02.09.18
  * */
-public class Client2 {
+public class Client02 {
     public static void main(String[] args) throws Exception {
-        String message;
-        char FLAG = 'F';
-        char header = 'H';
-        char trailer = 'T';
-        char ESC = 'E';
-        String flag = "01111110";
         Socket sock = new Socket("127.0.0.1", 2000);
         PrintWriter pw = new PrintWriter(sock.getOutputStream(), true);
         BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-        List<Character> al = Arrays.asList(FLAG,header,trailer,ESC);
         while(true) {
-        	String received = "";
+        	String received ;
         	if(br.ready()){
         		received = br.readLine();
             	if(received.equals("bye")) break;
-            	System.out.println("Received data "+received);
+            	System.out.println("Received data "+received+"\n");
             }
         }
         sock.close();
--- GoBackN/Server0.java
+++ GoBackN/Server0.java
@@ -1,14 +1,19 @@
-package DCCN.arq;
+package DCCN.GoBackN;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
 /*
 * Created by Rebecca D'souza on 02.09.18
 * */
-public class Server {
+public class Server0 {
     public static void main(String[] args) throws Exception {
-        String message;
-        System.out.println("Server is up");
+        String message,prev="";
+        System.out.println("Server is up\n");
         ServerSocket serverSocket1 = new ServerSocket(3000);
         Socket sock1 = serverSocket1.accept( );
         ServerSocket serverSocket2 = new ServerSocket(2000);
@@ -17,6 +22,8 @@ public class Server {
         PrintWriter pw2 = new PrintWriter(sock2.getOutputStream(), true);
         BufferedReader br1 = new BufferedReader(new InputStreamReader(sock1.getInputStream()));
         BufferedReader br2 = new BufferedReader(new InputStreamReader(sock2.getInputStream()));
+        Random random = new Random();
+        int delay;
 
         do {
             message = br1.readLine();
@@ -26,18 +33,18 @@ public class Server {
             	pw2.flush();
             	break;
             }
-            if(Integer.parseInt(String.valueOf(message.charAt(message.length()-1)))%2==1){
-            	System.out.println("Sending data "+message+" to client 2");
-            	pw2.println(message);
-            	pw2.flush();
-            	System.out.println("Sending "+"ack" + message.charAt(message.length()-1));
-            	pw1.println("ack" + message.charAt(message.length()-1));
-            	pw1.flush();
-            }
-            else{
-            	System.out.println("Not sending data "+message+" to client 2");
-            }
+            System.out.println("Sending data "+message+" to client 2");
+            pw2.println(message);
+            pw2.flush();
+            if(prev.equals(message)) delay = 0;
+            else delay = (random.nextInt(10)+5);
+            System.out.println("Sending "+"ack" + message.charAt(message.length()-1)+" after "+delay+" seconds\n");
+            try { Thread.sleep(delay*1000); }catch (Exception ignored){ }
+            pw1.println("ack" + message.charAt(message.length()-1));
+            pw1.flush();
+            prev = message;
         } while (!message.equals("bye"));
+
 
         serverSocket1.close();
         serverSocket2.close();
